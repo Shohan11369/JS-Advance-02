1537b830b26ceff248095f61f0aa7cf074b9f9c6
Shohan111369 <sarkershohan346@gmail.com>
2025-07-03 19:49:42 +0800

    Serial-Add

---
+++ index.js
@@ -0,0 +1,29 @@
+// Start
+
+document.addEventListener("DOMContentLoaded", () => {
+  const taskInput = document.getElementById("task-input");
+  const addBtn = document.getElementById("add-btn");
+  const todoBody = document.getElementById("todo-body");
+  const totalCount = document.getElementById("total");
+  const completedCount = document.getElementById("completed");
+  const pendingCount = document.getElementById("pending");
+
+  let tasks = [];
+
+  function updatedCounts() {
+    totalCount.textContent = taskInput.length;
+    completedCount.textContent = tasks.filter((t) => t.done).length;
+    pendingCount.textContent = tasks.filter((t) => !t.done).length;
+  }
+
+  function renderTasks() {
+    todoBody.innerHTML = "";
+    tasks.forEach((task, index) => {
+      const row = document.createElement("tr");
+
+      //  serial
+
+      row.innerHTML = `<td class="py-2" >${index + 1} </td>`;
+    });
+  }
+});
